92fb0bbaa2330c0585c2bbb5633697395541c1f0
Safaa <safaeettalhi@gmail.com>
2024-10-01 13:09:16 +0100

    add file js  for  tp concatine two chaine de caractere

---
+++ tp string/tp1.js
@@ -0,0 +1,17 @@
+  /*  ch1=prompt("donner une chaine");
+    ch2=prompt("donner une autre chaine");
+    if(ch1==ch2){
+        alert("les deux chaines sont identiques");
+    }else{
+        alert("chaine non identiques")
+    }*/
+//or
+function verif(){
+    let ch1=document.getElementById("ch1").value;
+    let ch2=document.getElementById("ch2").value;
+    if(ch1==ch2){
+        alert("les deux chaines sont identiques");
+    }else{
+        alert("chaine non identiques")
+    }
+}
