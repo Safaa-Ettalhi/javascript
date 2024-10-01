56ee2d34b2a6330da5e4e2af5cde7864b10ab977
Safaa <safaeettalhi@gmail.com>
2024-10-01 13:14:01 +0100

    add file js for tp compare chaine de caractere saisie avec une value donner

---
+++ tp string/tp2.js
@@ -0,0 +1,10 @@
+function tester(){
+    let ch1=document.getElementById("ch");
+    if(ch1.value=='javascript'){
+        alert('bravo')
+    }else{
+        alert('attention! ecrit "javascript"');
+        ch.value="";
+        ch.focus();
+    }
+}
