4a2cc0d140295a78d75d86f61339067f02c37358
Safaa <safaeettalhi@gmail.com>
2024-10-01 13:16:12 +0100

    update value saisie to lower case

--- tp string/tp2.js
+++ tp string/tp2.js
@@ -1,6 +1,6 @@
 function tester(){
     let ch1=document.getElementById("ch");
-    if(ch1.value=='javascript'){
+    if(ch1.value.toLowerCase()=='javascript'){
         alert('bravo')
     }else{
         alert('attention! ecrit "javascript"');
